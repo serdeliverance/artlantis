5b12522fa40eb21ba67d47881a1f69c2726ef407
sergio cano <serdeliverance@gmail.com>
2015-10-20 23:32:32 -0300

    se agrega campo description a user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151013023708) do
+ActiveRecord::Schema.define(version: 20151021022902) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20151013023708) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.text     "description"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
